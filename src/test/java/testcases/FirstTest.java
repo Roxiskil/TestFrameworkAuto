b77fd28e1da432f2b9e2516821848785d3435e06
Roxiskil <roxiskil@gmail.com>
2023-06-16 21:39:47 +0200

    Testcases_lesson2

--- src/test/java/testcases/FirstTest.java
+++ src/test/java/testcases/FirstTest.java
@@ -1,18 +1,42 @@
 package testcases;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
-
 public class FirstTest {
+    @Test
+    public void startDriver() {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\GitHub\\TestFrameworkAuto\\src\\test\\Resources\\executables\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("https://mvnrepository.com/open-source/testing-frameworks");
+    }
+    @Test
+public void openWebsite() {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\GitHub\\TestFrameworkAuto\\src\\test\\Resources\\executables\\chromedriver.exe");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        ChromeDriver driver = new ChromeDriver(options);
+        driver.get("http://bestbuy.com");
+    }
 
     @Test
-    public void openBrowser(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\IdeaProjects\\TestAutomationFramework/src/test/resources/executables/chromedriver.exe");
-        ChromeDriver driver = new ChromeDriver();
-        driver.get("https://mvnrepository.com/");
-        driver.manage().window().maximize();
-        driver.close();
+public void openSignInPage(){
+    System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Documents\\GitHub\\TestFrameworkAuto\\src\\test\\Resources\\executables\\chromedriver.exe");
 
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
 
-    }
+    ChromeDriver driver = new ChromeDriver(options);
+    driver.get("http://bestbuy.com");
+
+    driver.findElement(By.xpath(xpathExpression: "//span[text()='Account']")).click();
+
 }
