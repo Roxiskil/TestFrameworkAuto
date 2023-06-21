e26807ce4ee4ea31fd8d4bae0eb8c7ed9625a1b8
Roxiskil <roxiskil@gmail.com>
2023-06-22 00:18:05 +0200

    Home-work3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,55 @@
 package org.example;
 
+//HomeWork
+
+/* 1. username or firstname
+age
+location (state or city)
+product name
+price*/
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+       /* String firstName = "Becky";
+        String productName = "Wooden World";
+        String location = "Santa Fe";
+        int age = 25;
+        int discount = 20;
+        double price = 99.99;
+
+        System.out.println("Dear " + firstName + "! We would like to inform you that the Folk Art Market in " + location + " will be offering special discount of " + discount + "% on " + productName + " artworks worth over $" + price + " for those under " + age + " years old. We look forward to seeing you there.");
+
+    } */
+
+        // 2. Make 4 different math operations using int data variable. Print your result to console.
+
+
+        int product1 = 450;
+        int product2 = 98;
+        int product3 = 149;
+
+       // System.out.println(product1 + product2 - product3);
+
+        int number1 = 100;
+        int number2 = 0;
+        int number3 = 6;
+
+       // System.out.println(number1 + number3 / number2);
+
+        int a = 25;
+        int b = 4;
+
+        // System.out.println(a * b + a - b);
+
+        int x = 10000;
+        int y = 60;
+        int z = 5;
+
+        System.out.println(x / z + z * y);
     }
 }
+
+
+
+
